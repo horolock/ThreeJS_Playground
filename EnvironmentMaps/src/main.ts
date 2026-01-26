782e6e14327b7ff1e912ac1cc1d7f44fe73de267
horolock <horolock96@gmail.com>
2026-01-26 23:51:25 +0900

    Add Imports

--- EnvironmentMaps/src/main.ts
+++ EnvironmentMaps/src/main.ts
@@ -1,3 +1,10 @@
 import './style.css'
 import * as THREE from 'three'
+
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+
+import Stats from 'three/addons/libs/stats.module.js'
+
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js'
+
